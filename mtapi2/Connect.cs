209812a3ab18419bfec1fa7cfa8e02ee91346524
ponderela <p.charoenraksa@gmail.com>
2017-12-17 15:59:59 +0700

    h4 graph for trading ,interval every 1 hour,split connection , remove disconnect

--- mtapi2/Connect.cs
+++ mtapi2/Connect.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MT4bot;
 using System.Drawing;
+using Newtonsoft.Json;
 
 namespace mtapi2
 {
@@ -15,86 +16,88 @@ namespace mtapi2
     {
         readonly List<Action> _groupOrderCommands = new List<Action>();
         readonly MtApiClient _apiClient = new MtApiClient();
-        readonly TimerTradeMonitor _timerTradeMonitor;
-        readonly TimeframeTradeMonitor _timeframeTradeMonitor;
-        String previous = "";
-        int counter = 0;
+       // readonly TimerTradeMonitor _timerTradeMonitor;
+        //readonly TimeframeTradeMonitor _timeframeTradeMonitor;
+        int status = 0;
 
-        public Connect()
+        String symbol = "GBPUSD";
+        double volume = 0.02;
+        int slippage = 1;
+        double sl_volume = 0.010;
+        double tp_volume = 0.020;
+
+        /*status 1 = buy
+         *status 2 = sell 
+         */
+        public Connect(int status)
         {
-            //_timerTradeMonitor = new TimerTradeMonitor(_apiClient) { Interval = 10000 }; // 10 sec     
-
+            //_timerTradeMonitor = new TimerTradeMonitor(_apiClient) { Interval = 10000 }; // 10 sec
             //_timeframeTradeMonitor = new TimeframeTradeMonitor(_apiClient);
-
-            Connect_Click();
+            this.status = status;
+            Connect_Click();           
         }
 
-        private void Connect_Click()
+        public void Connect_Click()
         {
-
             //_timerTradeMonitor.Start();
             //_timeframeTradeMonitor.Start();
             _apiClient.BeginConnect(8222);
             apiClient_ConnectionStateChanged();
-
         }
 
-        private void apiClient_ConnectionStateChanged()
+        public void apiClient_ConnectionStateChanged()
         {
             while (true)
             {
-                var temp = _apiClient.ConnectionState;
-                if (temp.ToString() =="Connected")
-                {
-                    Console.WriteLine("Connected at : {0}",DateTime.Now.ToString());
-                    Interval();
-
-                }
-                else
-                {
-                    Console.WriteLine("Connecting");
-                }
-
-                Thread.Sleep(10000);
+                    var temp = _apiClient.ConnectionState;
+                    if (temp.ToString() == "Connected")
+                    {
+                        Console.WriteLine("Connected at : {0}", DateTime.Now.ToString());
+                        if(status == 1)
+                        {
+                            CloseAll();
+                             buy();
+                        }
+                        if(status == 2)
+                        {
+                             CloseAll();
+                            sell();
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Connecting");
+                    }
+                
+                Thread.Sleep(3000);
             }
         }
-
         public async void buy()
         {
-            String symbol = "GBPUSD";
-            double volume = 0.01;
-            int slippage = 1;
-            double sl_volume = 0.001;
-            double tp_volume = 0.002;
-
             var result = await Execute(() => _apiClient.SymbolInfoTick(symbol));
             double current = result.Ask;
 
 
-            // var ticket = await Execute(() => _apiClient.OrderSendBuy(symbol, volume, slippage, current - sl_volume, current + tp_volume));
-            var ticket = await Execute(() => _apiClient.OrderSendBuy(symbol, volume, slippage));
+             var ticket = await Execute(() => _apiClient.OrderSendBuy(symbol, volume, slippage, current - sl_volume, current + tp_volume));
+            //var ticket = await Execute(() => _apiClient.OrderSendBuy(symbol, volume, slippage));
             Console.WriteLine("Perform BUY Order at {0}",DateTime.Now.ToString());
         }
 
         public async void sell()
         {
-            String symbol = "GBPUSD";
-            double volume = 0.01;
-            int slippage = 1;
-            double sl_volume = 0.001;
-            double tp_volume = 0.002;
-
             var result = await Execute(() => _apiClient.SymbolInfoTick(symbol));
             double current = result.Bid;
 
 
-            //var ticket = await Execute(() => _apiClient.OrderSendSell(symbol, volume, slippage, current + sl_volume, current - tp_volume));
-            var ticket = await Execute(() => _apiClient.OrderSendSell(symbol, volume, slippage));
+            var ticket = await Execute(() => _apiClient.OrderSendSell(symbol, volume, slippage, current + sl_volume, current - tp_volume));
+            //var ticket = await Execute(() => _apiClient.OrderSendSell(symbol, volume, slippage));
             Console.WriteLine("Perform SELL Order at {0}", DateTime.Now.ToString());
         }
 
         public async void CloseAll()
         {
+            Thread.Sleep(10000);
             Console.WriteLine("Close Exist Order");
             var ticket = await Execute(() => _apiClient.OrderCloseAll());
         }
@@ -120,46 +123,6 @@ namespace mtapi2
                 return result;
             });
         }
-
-        public void Interval()
-        {
-           int  topThreshold = 110;
-           int botThreshold = 855;
-
-            img bitmapClass = new img(topThreshold, botThreshold);
-                bitmapClass.screenshot();
-                using (Bitmap bmp = new Bitmap("screen.bmp"))
-                {
-                    var scanning = bitmapClass.scanning(bmp);
-                    if (counter == 0)
-                    {
-                        previous = scanning;
-                        Console.WriteLine("First Initate initial state :{0}", previous);
-                    }
-                    else
-                    {
-                        if (scanning == "G" && previous != "G")
-                        {
-                            Console.WriteLine("Statechange from {0} to {1} BUY Signal", previous, scanning);
-                        //Console.Beep();
-                        CloseAll();
-                        Thread.Sleep(10000);
-                        buy();
-                            previous = scanning;
-                        }
-                        else if (scanning == "P" && previous != "P")
-                        {
-                            Console.WriteLine("Statechange from {0} to {1} SELL signal", previous, scanning);
-                        //Console.Beep();
-                        CloseAll();
-                        Thread.Sleep(10000);
-                        sell();
-                            previous = scanning;
-                        }
-                    }
-                }
-                counter++;
-            }
         }
     }
 
--- mtapi2/Program.cs
+++ mtapi2/Program.cs
@@ -3,14 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+using MT4bot;
+using Newtonsoft;
 
 namespace mtapi2
 {
     class Program
     {
+        static String previous = "";
+        static int counter = 0;
+
         static void Main(string[] args)
         {
-            Connect con = new Connect();
+            //Connect con = new Connect(1);
+            //Connect con2 = new Connect(2);
+
+            while (true)
+            {
+                var time = DateTime.Now.Minute.ToString();
+               
+
+                if(counter == 0)
+                {
+                    Console.WriteLine("Program start at {0}", DateTime.Now.ToString());
+                    Interval();
+                }
+
+                if (isTime(time))
+                {
+                    Interval();
+                }
+                Thread.Sleep(60*1000);
+            }
+        }
+        public static bool isTime(String minutes)
+        {
+            bool flag =false;
+            if ( minutes == "1")
+            {
+                Console.WriteLine("Hit time Condition at {0}", DateTime.Now.ToString());
+                flag = true;
+            }
+
+            //Console.WriteLine("not match time Condition at {0}", DateTime.Now.ToString());
+            return flag;
+        }
+
+        public static void Interval()
+        {
+            int topThreshold = 110;
+            int botThreshold = 855;
+
+            img bitmapClass = new img(topThreshold, botThreshold);
+            bitmapClass.screenshot();
+            using (Bitmap bmp = new Bitmap("screen.bmp"))
+            {
+                var scanning = bitmapClass.scanning(bmp);
+                if (counter == 0)
+                {
+                    previous = scanning;
+                    Console.WriteLine("First Initate initial state :{0}", previous);
+                }
+                else
+                {
+                    if (scanning == "B" && previous != "B")
+                    {
+                        Console.WriteLine("Statechange from {0} to {1} BUY Signal", previous, scanning);
+                        Connect con = new Connect(1);
+                        previous = scanning;
+                    }
+                    else if (scanning == "S" && previous != "S")
+                    {
+                        Console.WriteLine("Statechange from {0} to {1} SELL signal", previous, scanning);
+                        Connect con = new Connect(2);
+                        previous = scanning;
+                    }
+                }
+            }
+            counter++;
         }
     }
 }
